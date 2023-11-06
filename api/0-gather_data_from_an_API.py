0791a1486718d2753ee7f0d39b84e59d13ab4ec8
root <k.irumva@alustudent.com>
2023-11-06 12:52:34 +0000

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
-import requests
+"""
+A script to gather information about an employee's TODO list progress
+from a REST API.
+
+Usage: python3 script_name.py <employee_id>
+
+Requirements:
+- You must have the requests module installed.
+- Replace script_name.py with the actual script filename.
+"""
+
 import sys
+import requests
 
 def get_employee_todo_list_progress(employee_id):
     """
     Retrieve and display information about an employee's TODO list progress.
-    
+
     Args:
         employee_id (int): The employee's ID.
     """
@@ -16,20 +27,22 @@ def get_employee_todo_list_progress(employee_id):
 
     try:
         # Fetch employee data
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
+        response = requests.get(employee_url)
+        response.raise_for_status()
+        employee_data = response.json()
         employee_name = employee_data.get("name", "Unknown Employee")
 
         # Fetch TODO list for the employee
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
+        response = requests.get(todos_url)
+        response.raise_for_status()
+        todos_data = response.json()
 
         # Calculate progress
         total_tasks = len(todos_data)
-        completed_tasks = sum(1 for todo in todos_data if todo.get("completed", False))
+        completed_tasks = sum(1 for todo in todos_data if todo.get("completed"))
 
         # Print progress information
-        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
         print(f"{employee_name}: {employee_name}")
         print(f"NUMBER_OF_DONE_TASKS: {completed_tasks}")
         print(f"TOTAL_NUMBER_OF_TASKS: {total_tasks}")
@@ -37,7 +50,7 @@ def get_employee_todo_list_progress(employee_id):
         # Print the titles of completed tasks
         print("Completed tasks:")
         for todo in todos_data:
-            if todo.get("completed", False):
+            if todo.get("completed"):
                 print(f"\t{todo.get('title', 'Untitled Task')}")
 
     except requests.exceptions.RequestException as e:
@@ -47,8 +60,9 @@ def get_employee_todo_list_progress(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
+        print("Usage: python3 script_name.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
     get_employee_todo_list_progress(employee_id)
+
